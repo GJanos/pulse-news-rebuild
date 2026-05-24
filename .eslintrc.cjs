2d9450411fba5b9fe9eb073325fa38ca0741d676
GJanos <janos.gorondi.official@gmail.com>
2026-05-24 11:19:48 +0200

    chore: add root ESLint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    node: true,
+    es2020: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    'no-console': 'warn',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.expo/',
+    'android/',
+    '*.config.js',
+    '*.config.cjs',
+  ],
+};
